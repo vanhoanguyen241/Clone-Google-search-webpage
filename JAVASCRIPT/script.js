d520bf1d4fa12ba95163b5e8ea0a2b67b7c3ad2b
Hoà <vanhoanguyen241@gmail.com>
2026-02-25 14:36:03 +0700

    feat(nav): Thêm menu đổ xuống cho nút 9 chấm
    - Thêm các ảnh vào thư mục dự án để chèn vào menu đổ xuống
    - Cập nhật các file html để thêm menu cho mỗi ngôn ngữ
    - Chỉnh sửa file css để định dạng cho menu được thêm vào
    - Thêm các sự kiện logic vào file javascript để menu hoạt động

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -8,3 +8,27 @@ document.getElementById("search_input").addEventListener("keydown", function(eve
         }
     }
 });
+
+const menu = document.getElementsByClassName("menu")[0];
+const dropdown = document.getElementsByClassName("dropdown_menu_container")[0];
+
+menu.addEventListener("click", function() {
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+        menu.style.backgroundColor = "hsl(0, 0%, 90%)";
+    } else {
+        dropdown.style.display = "none";
+        menu.style.backgroundColor = "transparent";
+    }
+});
+
+dropdown.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+});
+
+document.addEventListener("click", function(event) {
+    if (!menu.contains(event.target) && !dropdown.contains(event.target)) {
+    dropdown.style.display = "none";
+    menu.style.backgroundColor = "transparent";
+    }
+});
