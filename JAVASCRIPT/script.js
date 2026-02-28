3008e445054133698a2a6f802de0c0c28749096a
Hoà <vanhoanguyen241@gmail.com>
2026-02-28 13:44:03 +0700

    feat(footer): Thêm menu cài đặt nổi khi click vào
    - Chỉnh sửa các file html để thêm menu
    - Cập nhật file css để định dạng menu này
    - Cập nhật file javascript để xử lí logic

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -32,3 +32,55 @@ document.addEventListener("click", function(event) {
     menu.style.backgroundColor = "transparent";
     }
 });
+
+const settings = document.getElementsByClassName("settings")[0];
+const settingsMenu = document.getElementsByClassName("settings_menu_container")[0];
+const darkModeOff = document.querySelector(".dark_mode_off");
+const darkModeOn = document.querySelector(".dark_mode_on");
+
+settings.addEventListener("click", function() {
+    if (settingsMenu.style.display === "none" || settingsMenu.style.display === "") {
+        settingsMenu.style.display = "block";
+    } else {
+        settingsMenu.style.display = "none";
+    }
+});
+
+settingsMenu.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+});
+
+document.addEventListener("click", function(event) {
+    if (!settings.contains(event.target) && !settingsMenu.contains(event.target)) {
+    settingsMenu.style.display = "none";
+    }
+});
+
+darkModeOff.addEventListener("click", function() {
+    darkModeOff.style.display = "none";
+    darkModeOn.style.display = "flex";
+    document.body.classList.add("dark_mode");
+});
+
+darkModeOn.addEventListener("click", function() {
+    darkModeOn.style.display = "none";
+    darkModeOff.style.display = "flex";
+    document.body.classList.remove("dark_mode");
+});
+
+const sendReport = document.querySelector(".send_report");
+
+sendReport.addEventListener("click", function() {
+    if (document.body.classList.contains("english_page")) {
+        alert("Notice: This feature is currently not supported.");
+    }
+    else if (document.body.classList.contains("france_page")) {
+        alert("Avis: Cette fonctionnalité n'est pas encore prise en charge.");
+    }
+    else if (document.body.classList.contains("chinese_page")) {
+        alert("通知：此功能目前尚不受支持。");
+    }
+    else {
+        alert("Thông báo: Tính năng này hiện chưa được hỗ trợ.");
+    }
+});
